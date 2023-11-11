8474ebc40275bda5c6441dd553553bd8b4addfc2
alitr53 <47153942+alitr53@users.noreply.github.com>
2023-11-11 14:45:29 +0300

    Delete utils directory

--- utils/logger.js
+++
@@ -1,7 +0,0 @@
-const moment = require("moment");
-module.exports = class Logger {
-	static log (content) {
-		const date = `${moment().format("DD-MM-YYYY hh:mm:ss")}`;
-		 console.log(`[${date}] ${content}`);
-	}
-};
